db5f2347b6b9d50f6e187a74f16f4b297a596e3e
SaracensFan <matthewclifford@hotmail.co.uk>
2022-11-27 13:33:46 +0000

    update to router and models

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ let port = 9000
 
 mongoose.connect(process.env.MONGO_DB).then(console.log('Connected to ARC DB')).catch(err => console.error(err))
 
-
-const Download = require('./models/downloadSchema')
-
+const downloadSchema = require('./routes/downloadRouter')
 const webinarRouter = require('./routes/webinarRouter')
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -20,38 +18,25 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.header({
         "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT"
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT"
     });
     next();
   });
 
+// Index
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+// Resource downloads
 app.route('/api/resource')
-.get(async (req, res) => {
-    const download = await Download.find()
-    try {
-        res.send(download)
-    } catch (err) {
-        console.error(err)
-    }
-})
-.post(async (req, res) => {
-    const newDownload = new Download({
-        name: req.body.fullName,
-        email: req.body.email
-    })
-    const saveDownload = await newDownload.save()
-    res.send(req.body)
-})
+.get(downloadSchema)
+.post(downloadSchema)
 
 // Webinar
 app.route('/api/webinar')
 .get(webinarRouter)
 .post(webinarRouter)
-
 app.route('/api/webinar/update')
 .put(webinarRouter)
 
---
+++ routes/downloadRouter.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+const Download = require('../models/downloadSchema')
+
+router.route('/api/resource')
+.get(async (req, res) => {
+    const download = await Download.find()
+    try {
+        res.send(download)
+    } catch (err) {
+        console.error(err)
+    }
+})
+.post(async (req, res) => {
+    const newDownload = new Download({
+        name: req.body.fullName,
+        email: req.body.email
+    })
+    const saveDownload = await newDownload.save()
+    res.send(req.body)
+})
+
+module.exports = router
